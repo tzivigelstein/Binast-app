6495e955fc5ef648b8aa769f8bda159f65f3543d
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-04 16:15:37 -0300

    add context types

---
+++ context/types/index.js
@@ -0,0 +1,8 @@
+export const LOADING = 'LOADING';
+export const SET_ERROR = 'SET_ERROR';
+
+export const GET_CRYPTOS = 'GET_CRYPTOS';
+export const GET_CRYPTOS_SUCCESS = 'GET_CRYPTOS_SUCCESS';
+export const GET_CRYPTOS_ERROR = 'GET_CRYPTOS_ERROR';
+
+export const SET_SELECTED_CRYPTO = 'SET_SELECTED_CRYPTO';
