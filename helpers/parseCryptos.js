413c4c636024202370b080984e4a8bee8a875c26
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-04 16:07:00 -0300

    add parseCryptos to map api data

---
+++ helpers/parseCryptos.js
@@ -0,0 +1,69 @@
+import millify from 'millify'
+import priceStatus from './priceStatus'
+import numbro from 'numbro'
+
+export default function parseCryptos(cryptos) {
+  const parsedCryptos = cryptos.map(({ RAW, CoinInfo, DISPLAY }) => {
+    const {
+      USD: { IMAGEURL, PRICE, MKTCAP, CHANGEPCTDAY, OPENDAY, VOLUMEHOUR, LOWDAY, HIGHDAY },
+    } = RAW
+
+    const { Name, FullName } = CoinInfo
+
+    const {
+      USD: { FROMSYMBOL },
+    } = DISPLAY
+
+    const imageUrl = `https://www.cryptocompare.com/${IMAGEURL.slice(1)}`
+
+    const price = numbro(PRICE).formatCurrency({ thousandSeparated: true, currencySymbol: '$' })
+
+    const marketCapital = millify(MKTCAP, { precision: 2, space: true })
+
+    const changeDayPercentage = numbro(CHANGEPCTDAY / 100).format({
+      output: 'percent',
+      mantissa: 2,
+    })
+
+    const changeDayPrice = numbro((CHANGEPCTDAY * PRICE) / 100).format({
+      thousandSeparated: true,
+      trimMantissa: true,
+      mantissa: 2,
+      forceSign: true,
+    })
+
+    const openDay = OPENDAY.toLocaleString()
+    const volumeHour = `${FROMSYMBOL} ${millify(VOLUMEHOUR, {
+      precision: 2,
+      space: true,
+    })}`
+
+    const isUp =
+      PRICE > OPENDAY ? priceStatus.PRICE_UP : PRICE === OPENDAY ? priceStatus.PRICE_EQUAL : priceStatus.PRICE_DOWN
+
+    const highDay = `$ ${HIGHDAY.toLocaleString(undefined, {
+      maximumFractionDigits: HIGHDAY < 10 ? 2 : 0,
+    })}`
+
+    const lowDay = `$ ${LOWDAY.toLocaleString(undefined, {
+      maximumFractionDigits: LOWDAY < 10 ? 2 : 0,
+    })}`
+
+    return {
+      imageUrl,
+      price,
+      marketCapital,
+      changeDayPercentage,
+      changeDayPrice,
+      openDay,
+      volumeHour,
+      isUp,
+      highDay,
+      lowDay,
+      name: FullName,
+      id: Name,
+    }
+  })
+
+  return parsedCryptos
+}
