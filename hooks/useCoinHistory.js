b81b3d5956a6b9f5230358d59647fddce9565f9b
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-10-04 15:54:43 -0300

    add useCoinHistory for historical chart data

---
+++ hooks/useCoinHistory.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+
+const useCoinHistory = id => {
+  const [chartData, setChartData] = useState([])
+
+  useEffect(() => {
+    getCryptoHistory(id)
+  }, [id])
+
+  const getCryptoHistory = async (id = 'BTC') => {
+    const oneYear = 365
+    const url = `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${id}&tsym=USD&limit=${oneYear}`
+
+    try {
+      const query = await fetch(url)
+      const data = await query.json()
+      const res = await data.Data.Data
+
+      setChartData(
+        res.map(({ close, time }) => ({
+          x: time,
+          y: close,
+        }))
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return chartData
+}
+
+export default useCoinHistory
