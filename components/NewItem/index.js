2ee9a34ee362f669a415fd557a91f4a5e8f7aaa5
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-11-01 09:40:59 -0300

    Populate news item with API data and add styling

--- components/NewItem/index.js
+++ components/NewItem/index.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
-import { Text, TouchableWithoutFeedback, View } from 'react-native'
+import React from 'react'
+import { Text, TouchableWithoutFeedback, View, Image } from 'react-native'
 import * as Link from 'expo-linking'
 import styleCreator from './styles'
-import theme from '../../theme'
+import useTheme from '../../hooks/useTheme'
+import { Web } from '../Icons'
 
 const NewItem = ({ item }) => {
-  const { id, title, url, source, published_at } = item
+  const { id, title, description, publisher, url, thumbnail, date } = item
+
+  const theme = useTheme()
 
   const styles = styleCreator(theme)
 
@@ -14,9 +17,22 @@ const NewItem = ({ item }) => {
   }
 
   return (
-    <TouchableWithoutFeedback onFocus={() => console.log('focus')} onPress={handleItemPress}>
+    <TouchableWithoutFeedback onPress={handleItemPress}>
       <View style={styles.newItemContainer}>
-        <Text>{title}</Text>
+        {thumbnail && <Image style={styles.newImage} source={{ uri: thumbnail }} />}
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.newDataContainer}>
+          <View style={styles.infoContainer}>
+            <Text style={styles.sourceTitle} numberOfLines={1}>
+              {publisher}
+            </Text>
+            <Text style={styles.newDate}> - {date}</Text>
+          </View>
+          <View style={styles.visualButton}>
+            <Text style={styles.visualButtonText}>Open</Text>
+            <Web width={16} height={16} fill="#fff" />
+          </View>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   )
--- components/NewItem/styles.js
+++ components/NewItem/styles.js
@@ -1,11 +1,65 @@
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
+
+const { width, height } = Dimensions.get('window')
+
+const aspectRatio = width / height
 
 export default styleCreator = theme =>
   StyleSheet.create({
     newItemContainer: {
-      paddingVertical: 4,
+      paddingVertical: 10,
       paddingHorizontal: 3,
+      marginBottom: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: theme.secondary,
+    },
+
+    newImage: {
+      width: '100%',
+      height: width * aspectRatio,
+    },
+
+    newDataContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+
+    title: {
+      color: theme.text,
+      fontSize: 16,
       marginBottom: 5,
-      backgroundColor: 'green',
+    },
+
+    infoContainer: {
+      flexDirection: 'row',
+    },
+
+    sourceTitle: {
+      color: theme.textSecondary,
+      fontSize: 15,
+      maxWidth: '70%',
+      overflow: 'hidden',
+    },
+
+    newDate: {
+      color: theme.textSecondary,
+      fontSize: 15,
+    },
+
+    visualButton: {
+      borderRadius: 999,
+      backgroundColor: theme.primary,
+      paddingHorizontal: 8,
+      paddingVertical: 6,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+
+    visualButtonText: {
+      color: theme.alternativeTextColor,
+      fontWeight: 'bold',
+      fontSize: 15,
+      marginRight: 5,
     },
   })
